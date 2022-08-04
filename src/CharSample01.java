6e3708fb6e9af5a01678e0a7bf0d8f60d25608fc
latency707 <latency707@gmail.com>
2022-08-04 16:00:17 +0900

    .

---
+++ src/CharSample01.java
@@ -0,0 +1,12 @@
+public class CharSample01 {
+    public static void main(String[] args) {
+        char a1 = 'a'; // 문자로 a를 표현
+        char a2 = 97; // 아스키코드로 a를 표현
+        char a3 = '\u0061'; // 유니코드로 a를 표현
+
+        System.out.println(a1); // a
+        System.out.println(a2); // a
+        System.out.println(a3); // a
+
+    }
+}
