28e0407cc8f94e7b09f4e8d1aaf073256b4788c4
latency707 <latency707@gmail.com>
2022-08-04 13:41:48 +0900

    .

---
+++ src/IncreDecreSample01.java
@@ -0,0 +1,12 @@
+// 증감연산 예시
+public class IncreDecreSample01 {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 10;
+        i++;
+        j--;
+
+        System.out.println(i); // 1 출력
+        System.out.println(j); // 9 출력    
+    }
+}
---
+++ src/IncreDecreSample02.java
@@ -0,0 +1,8 @@
+public class IncreDecreSample02 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i++); // 0
+        System.out.println(i); // 1
+    }
+    
+}
