83ed1ebbb77fee6b30ecefd5f7bb8d9882b75f10
latency707 <latency707@gmail.com>
2022-08-04 16:00:45 +0900

    .

---
+++ src/BoolSample01.java
@@ -0,0 +1,12 @@
+public class BoolSample01 {
+    public static void main(String[] args) {
+        int base = 180;
+        int height = 185;
+        boolean isTall = height > base;
+
+        if (isTall) {
+            System.out.println("키가 큽니다.");
+        }
+    }
+    
+}
---
+++ src/BoolSample02.java
@@ -0,0 +1,8 @@
+public class BoolSample02 {
+    public static void main(String[] args) {
+        int i = 3;
+        boolean isOdd = i % 2 == 1;
+        System.out.println(isOdd); // true
+    }
+    
+}
