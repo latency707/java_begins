0cd7e0b8b95bea8d9824075bc31245d42bc586d6
latency707 <latency707@gmail.com>
2022-08-04 16:34:20 +0900

    .

---
+++ src/EqualsSample01.java
@@ -0,0 +1,10 @@
+    public class EqualsSample01 {
+        public static void main(String[] args){
+            String a = "hello";
+            String b = "java";
+            String c = "hello";
+            System.out.println(a.equals(b)); // false
+            System.out.println(a.equals(c)); // true
+        }
+        
+    }
---
+++ src/EqualsSample02.java
@@ -0,0 +1,9 @@
+public class EqualsSample02 {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = new String("hello");
+        System.out.println(a.equals(b)); // true
+        System.out.println(a == b); // false
+    }
+    
+}
