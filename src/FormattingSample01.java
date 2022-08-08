93c087c71b1640c22b54dfe7616f5b8eeb440a75
latency707 <latency707@gmail.com>
2022-08-08 14:18:05 +0900

    .

---
+++ src/FormattingSample01.java
@@ -0,0 +1,11 @@
+/*
+문자열 포매팅이란, 문자열 안에 어떤 특정 값을 삽입하는 방법
+*/
+
+// 숫자 바로 대입
+public class FormattingSample01 {
+    public static void main(String[] args) {
+        System.out.println(String.format("I eat %d apples.", 3)); // "I eat 3 apples." 출력
+
+    }
+}
